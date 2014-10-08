b395c82cfad71cdf9d89328a95e481eb83ab3ab2
domain1973 <domain_xu@hotmail.com>
2014-10-08 23:08:54 +0800

    初次提交

---
+++ android/build/generated/source/buildConfig/release/com/ads/puzzle/flag/android/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ads.puzzle.flag.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "com.ads.puzzle.flag.android";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "1.6";
+}
